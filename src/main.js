504e60168675607070642b59e6393a5c336f6da0
Nikola <nikola.dreznjak98@gmail.com>
2022-03-07 13:34:37 +0100

    update commit

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from './App';
 import router from './router';
 import store from './store';
 
-/* createApp(App).use(router).use(store).mount('#app'); */
 const app = createApp(App);
 app.use(router);
 app.use(store);
