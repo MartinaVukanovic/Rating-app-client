609f0fa54917be588a153e29c64764a5e5008649
Nikola <nikola.dreznjak98@gmail.com>
2022-03-09 08:10:32 +0100

    store update

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,11 @@
-export default {};
+import Axios from 'axios';
+
+export default {
+  async submitEmotion({ commit }, name) {
+    const response = Axios.post('http://localhost:3000/', {
+      name,
+    });
+
+    commit('submitEmotion', response);
+  },
+};
